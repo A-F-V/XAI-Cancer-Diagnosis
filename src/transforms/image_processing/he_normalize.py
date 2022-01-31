Fixed normalization through correcive recoluring

src/transforms/image_processing/he_normalize.py
@@ -12,7 +12,7 @@ def normalize_vec(vec: Tensor):
     return vec / vec.norm(p=2, dim=0)
 
 
-def get_stain_vectors(img: Tensor, alpha=0.01, beta=0.15, clipping=4, debug=False):
+def get_stain_vectors(img: Tensor, alpha=0.01, beta=0.15, clipping=10, debug=False):
     """Gets the stain vectors for an image in OD Space
         Implemets the Macenko et al. (2016) method for stain normalization.
     Args:
@@ -183,7 +183,7 @@ def normalize_he_image(img: Tensor, alpha=0.01, beta=0.15):  # todo create TESTS
         img (tensor): The RGB H&E image
     """
     v1, v2 = get_stain_vectors(img, alpha=0.01)
-    if abs(v1[0].item()-v2[0].item()) < 0.3 or min(v1[0].item(), v2[0].item()) > 0.4:
+    if abs(v1[0].item()-v2[0].item()) < 0.3 or min(v1[0].item(), v2[0].item()) > 0.3:
         print("Singular")
         return img  # This is for what I call singularly stained images
     standard_v1, standard_v2 = Tensor([0.7247, 0.6274, 0.2849]), Tensor([0.0624, 0.8357, 0.5456])
@@ -192,9 +192,15 @@ def normalize_he_image(img: Tensor, alpha=0.01, beta=0.15):  # todo create TESTS
     new_basis = torch.stack([standard_v1, standard_v2], dim=0).T
 
     flat_img = img.flatten(1, 2)
-    od = -torch.log10(flat_img).clip(0, 4)
+    od = (-torch.log10(flat_img)).clip(0, 100)
 
     new_od = new_basis @ torch.linalg.pinv(old_basis) @ od
-    new_od = new_od.unflatten(1, (img.shape[1], img.shape[2]))
+    new_od = new_od.unflatten(1, (img.shape[1], img.shape[2])).clip(0, 10)
     rgb = torch.pow(10, -new_od).clip(0, 1)  # !TODO A BETTER CLIPPING THAN THIS!
+    # corrective recolouring
+    e = 1/255
+    mask = torch.logical_not((rgb[0].ge(1.-e) & rgb[1].le(0.+e) & rgb[2].le(0.+e)) |
+                             (rgb[0].le(0.+e) & rgb[1].le(0.+e) & rgb[2].ge(1.-e))).int()
+    mask = mask.unsqueeze(0).repeat(3, 1, 1)
+    rgb = rgb*mask
     return rgb