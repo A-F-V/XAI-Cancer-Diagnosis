bc9df140dba3ef59d95555596809af89610635bb
A-F-V <af695@cam.ac.uk>
2022-02-05 17:14:27 +0000

    Tested and Working Counting Matrix

--- src/algorithm/counting_matrix.py
+++ src/algorithm/counting_matrix.py
@@ -5,19 +5,35 @@ class CountingMatrix:
         self.cum_matrix = [0]*width
         self.size = 0
         self.width = width
+        self._dirty = False
 
     def add(self, index):
-        self.matrx[index] += 1
+        self.matrix[index] += 1
+        self._dirty = True
 
     def add_many(self, index, amt):
         self.matrix[index] += amt
+        self._dirty = True
 
     def cumulate(self):
         for i in range(1, self.width):
             self.cum_matrix[i] = self.cum_matrix[i-1] + self.matrix[i-1]
+        self._dirty = False
 
     def __getitem__(self, index):
         # Binary Search Alogrithm
+        if self._dirty:
+            self.cumulate()
+        if index < 0 or index >= self.cum_matrix[-1]+self.matrix[-1]:
+            raise IndexError
+        bot, top = 0, self.width
+        while bot + 1 < top:
+            mid = (bot + top) // 2
+            if self.cum_matrix[mid] <= index:
+                bot = mid
+            else:
+                top = mid
+        return (bot, index-self.cum_matrix[bot])
 
     def __len__(self):
         return self.size
---
+++ tests/algorithm/test_counting_matrix.py
@@ -0,0 +1,25 @@
+from src.algorithm.counting_matrix import CountingMatrix
+
+
+def test_counting_matrix():
+    cm = CountingMatrix(4)
+    cm.add(0)
+    cm.add_many(1, 3)
+    cm.add_many(2, 4)
+    cm.add_many(3, 2)
+
+    assert cm[0] == (0, 0)
+    assert cm[3] == (1, 2)
+    assert cm[8] == (3, 0)
+
+    try:
+        cm[-1]
+        assert False
+    except IndexError:
+        assert True
+
+    try:
+        cm[10]
+        assert False
+    except IndexError:
+        assert True
