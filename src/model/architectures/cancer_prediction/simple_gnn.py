508cccf8a8b723e699d331537aa84bb65f790020
A-F-V <af695@cam.ac.uk>
2022-02-08 20:47:09 +0000

    Some good work towards grid search

--- src/model/architectures/cancer_prediction/simple_gnn.py
+++ src/model/architectures/cancer_prediction/simple_gnn.py
@@ -5,22 +5,36 @@ from torch.nn import ModuleList, Sequential, Linear, ReLU, BatchNorm1d, Softmax,
 from torch.nn.functional import nll_loss, sigmoid, log_softmax
 import torch
 from torch import optim, Tensor
+from src.transforms.graph_construction.graph_extractor import mean_pixel_extraction, principle_pixels_extraction
+
+
+def mean_pixel(X: Tensor):  # batched
+    img_size = int((X.shape[1]//3)**0.5)
+    pixel = X.unflatten(1, (3, img_size, img_size)).mean(dim=(2, 3))
+    return pixel
 
 
 class SimpleGNN(pl.LightningModule):
-    def __init__(self, img_size=30, layers=8, num_steps=0, train_loader=None, val_loader=None, **kwargs):
+
+    def reducer(self, name):
+        if name == "MEAN_PIXEL":
+            return mean_pixel, 3
+
+    def __init__(self, img_size=30, num_steps=0, train_loader=None, val_loader=None, **config):
         super(SimpleGNN, self).__init__()
-        self.args = kwargs
+        self.args = config
         self.img_size = img_size
-        self.learning_rate = kwargs["START_LR"]
+        self.learning_rate = config["START_LR"]
 
         self.num_steps = num_steps
         self.train_loader = train_loader
         self.val_loader = val_loader
 
+        self.node_reducer, node_dim = self.reducer(config["NODE_REDUCER"])
+
         # self.model = GIN(3*img_size**2, 3*img_size**2//4, num_layers=layers, dropout=0.8, out_channels=300)'
-        self.model = GCN(21, 5, num_layers=layers, dropout=kwargs["DROPOUT"], jk="cat", out_channels=5) if kwargs["ARCH"] == "GCN" else GIN(
-            21, 5, num_layers=layers, dropout=kwargs["DROPOUT"], out_channels=5)
+        self.model = GCN(node_dim, 5, num_layers=config["LAYERS"], dropout=config["DROPOUT"], out_channels=5) if config["ARCH"] == "GCN" else GIN(
+            node_dim, 5, num_layers=config["LAYERS"], dropout=config["DROPOUT"], out_channels=5)
         self.predictor = Sequential(
 
             # BatchNorm(300),
@@ -31,6 +45,7 @@ class SimpleGNN(pl.LightningModule):
         )
 
     def forward(self, x, edge_index, edge_attr, batch):
+        x = self.node_reducer(x)
         x = self.model(x, edge_index)  # , edge_weight=edge_attr)
         xp = global_mean_pool(x, batch)
         return self.predictor(xp)
@@ -69,7 +84,15 @@ class SimpleGNN(pl.LightningModule):
         self.log("val_loss", loss)
         self.log("val_acc", acc)
         self.log("val_canc_acc", canc_acc)
-        return loss
+        return {'val_loss': loss, 'val_acc': acc, 'val_canc_acc': canc_acc}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        avg_acc = torch.stack([x["val_acc"] for x in outputs]).mean()
+        avg_canc_acc = torch.stack([x["val_canc_acc"] for x in outputs]).mean()
+        self.log("ep/val_loss", avg_loss)
+        self.log("ep/val_acc", avg_acc)
+        self.log("ep/val_canc_acc", avg_canc_acc)
 
     def train_dataloader(self):
         return self.train_loader
